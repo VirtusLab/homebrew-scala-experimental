92332e3ebb6349bc4e550e86ff87ac2f41067433
gh-actions <actions@github.com>
2024-05-06 23:53:16 +0000

    Update for 1.3.1

--- scala.rb
+++ scala.rb
@@ -6,12 +6,12 @@ class Scala < Formula
   desc "Experimental launcher for Scala"
   homepage "https://virtuslab.github.io/scala-cli/"
   url (RUBY_PLATFORM.include? "arm64") ?
-    "https://github.com/Virtuslab/scala-cli/releases/download/v1.3.0/scala-cli-aarch64-apple-darwin.gz" :
-    "https://github.com/Virtuslab/scala-cli/releases/download/v1.3.0/scala-cli-x86_64-apple-darwin.gz"
-  version "1.3.0"
+    "https://github.com/Virtuslab/scala-cli/releases/download/v1.3.1/scala-cli-aarch64-apple-darwin.gz" :
+    "https://github.com/Virtuslab/scala-cli/releases/download/v1.3.1/scala-cli-x86_64-apple-darwin.gz"
+  version "1.3.1"
   sha256 (RUBY_PLATFORM.include? "arm64") ?
-    "9c1cf27fbe1326646a1ad69c73d3cd2ed6578f5abb0cabedb792a1cd70a51390" :
-    "de17cf51693c9fa3260a03feb6bc98b1135ee3c10f420e7b52f3c3b26994c835"
+    "538378ba7bb8f9957266fecbb404c06920c85da7950e237a25f57879dae3f112" :
+    "8010e5bea22b02de82913028c89e84be67b6c7f3b0c03e9d73c14935b54b44fa"
   license "Apache-2.0"
 
   def install
